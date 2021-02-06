33f5f5f1e2058ca24945d44c5c449b0a21de0754
adnanqazii <adnanqazi123@gmail.com>
2021-02-06 22:23:03 +0500

    backend

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,125 +1,104 @@
-import React, { useContext, useRef, useReducer } from "react";
-import {
-  Container,
-  Flex,
-  Heading,
-  Button,
-  Input,
-  Label,
-  NavLink,
-  Checkbox
-} from "theme-ui";
-import { Router, Link } from "@reach/router";
-import { gql, useMutation, useQuery } from "@apollo/client";
-import { IdentityContext } from "../../identity-context";
+const { ApolloServer, gql } = require("apollo-server-lambda");
+require("dotenv").config();
+const faunadb = require("faunadb");
+const q = faunadb.query;
 
-const ADD_TODO = gql`
-  mutation AddTodo($text: String!) {
-    addTodo(text: $text) {
-      id
-    }
+var client = new faunadb.Client({ secret: process.env.FAUNA });
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    todos: [Todo]!
+  }
+  type Todo {
+    id: ID!
+    text: String!
+    done: Boolean!
+  }
+  type Mutation {
+    addTodo(text: String!): Todo
+    updateTodoDone(id: ID!): Todo
   }
 `;
 
-const UPDATE_TODO_DONE = gql`
-  mutation UpdateTodoDone($id: ID!) {
-    updateTodoDone(id: $id) {
-      text
-      done
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    todos: async (parent, args, { user }) => {
+      if (!user) {
+        return [];
+      } else {
+        const results = await client.query(
+          q.Paginate(q.Match(q.Index("todos_by_user"), user))
+        );
+        return results.data.map(([ref, text, done]) => ({
+          id: ref.id,
+          text,
+          done
+        }));
+      }
     }
-  }
-`;
-
-const GET_TODOS = gql`
-  query GetTodos {
-    todos {
-      id
-      text
-      done
-    }
-  }
-`;
-
-const todosReducer = (state, action) => {
-  switch (action.type) {
-    case "addTodo":
-      return [{ done: false, value: action.payload }, ...state];
-    case "toggleTodoDone":
-      const newState = [...state];
-      newState[action.payload] = {
-        done: !state[action.payload].done,
-        value: state[action.payload].value
+  },
+  Mutation: {
+    addTodo: async (_, { text }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to insert todos");
+      }
+      const results = await client.query(
+        q.Create(q.Collection("todos"), {
+          data: {
+            text,
+            done: false,
+            owner: user
+          }
+        })
+      );
+      return {
+        ...results.data,
+        id: results.ref.id
       };
-      return newState;
+    },
+    updateTodoDone: async (_, { id }, { user }) => {
+      if (!user) {
+        throw new Error("Must be authenticated to insert todos");
+      }
+      const results = await client.query(
+        q.Update(q.Ref(q.Collection("todos"), id), {
+          data: {
+            done: true
+          }
+        })
+      );
+      return {
+        ...results.data,
+        id: results.ref.id
+      };
+    }
   }
 };
 
-export default () => {
-  const { user, identity: netlifyIdentity } = useContext(IdentityContext);
-  const [todos, dispatch] = useReducer(todosReducer, []);
-  const inputRef = useRef();
-  const [addTodo] = useMutation(ADD_TODO);
-  const [updateTodoDone] = useMutation(UPDATE_TODO_DONE);
-  const { loading, error, data, refetch } = useQuery(GET_TODOS);
-  return (
-    <Container>
-      <Flex as="nav">
-        <NavLink as={Link} to="/" p={2}>
-          Home
-        </NavLink>
-        <NavLink as={Link} to={"/app"} p={2}>
-          Dashboard
-        </NavLink>
-        {user && (
-          <NavLink
-            href="#!"
-            p={2}
-            onClick={() => {
-              netlifyIdentity.logout();
-            }}
-          >
-            Log out {user.user_metadata.full_name}
-          </NavLink>
-        )}
-      </Flex>
-      <Flex
-        as="form"
-        onSubmit={async e => {
-          e.preventDefault();
-          await addTodo({ variables: { text: inputRef.current.value } });
-          inputRef.current.value = "";
-          await refetch();
-        }}
-      >
-        <Label sx={{ display: "flex" }}>
-          <span>Add&nbsp;Todo</span>
-          <Input ref={inputRef} sx={{ marginLeft: 1 }} />
-        </Label>
-        <Button sx={{ marginLeft: 1 }}>Submit</Button>
-      </Flex>
-      <Flex sx={{ flexDirection: "column" }}>
-        {loading ? <div>loading...</div> : null}
-        {error ? <div>{error.message}</div> : null}
-        {!loading && !error && (
-          <ul sx={{ listStyleType: "none" }}>
-            {data.todos.map(todo => (
-              <Flex
-                key={todo.id}
-                as="li"
-                onClick={async () => {
-                  console.log("updateTodoDone");
-                  await updateTodoDone({ variables: { id: todo.id } });
-                  console.log("refetching");
-                  await refetch();
-                }}
-              >
-                <Checkbox checked={todo.done} readOnly />
-                <span>{todo.text}</span>
-              </Flex>
-            ))}
-          </ul>
-        )}
-      </Flex>
-    </Container>
-  );
-};
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ context }) => {
+    if (context.clientContext.user) {
+      return { user: context.clientContext.user.sub };
+    } else {
+      return {};
+    }
+  },
+  // By default, the GraphQL Playground interface and GraphQL introspection
+  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+  //
+  // If you'd like to have GraphQL Playground and introspection enabled in production,
+  // the `playground` and `introspection` options must be set explicitly to `true`.
+  playground: true,
+  introspection: true
+});
+
+exports.handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true
+  }
+});
--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,4 +1,6 @@
 const React = require("react");
+
+const { setContext } = require("apollo-link-context");
 const {
   ApolloProvider,
   ApolloClient,
@@ -7,13 +9,26 @@ const {
 } = require("@apollo/client");
 const wrapRootElement = require("./wrap-root-element");
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
+const authLink = setContext((_, { headers }) => {
+    const user = netlifyIdentity.currentUser();
+    const token = user.token.access_token;
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        Authorization: token ? `Bearer ${token}` : ""
+      }
+    };
+  });
+  
+  const httpLink = new HttpLink({
     uri:
-      "https://dazzling-feynman-41a653.netlify.app/.netlify/functions/graphql"
-  })
-});
+      "https://serverless-todo-netlify-fauna-egghead.netlify.com/.netlify/functions/graphql"
+  });
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink)
+  });
 
 exports.wrapRootElement = ({ element }) => {
   return (

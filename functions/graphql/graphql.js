49a45c7d8daf5ba8066960d5a38cec1ad5500bf4
adnanqazii <adnanqazi123@gmail.com>
2021-02-06 21:23:56 +0500

    gql and cors,refetch
    gql provider should be in gatsby-browser only.

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,55 +1,125 @@
-const { ApolloServer, gql } = require("apollo-server-lambda");
+import React, { useContext, useRef, useReducer } from "react";
+import {
+  Container,
+  Flex,
+  Heading,
+  Button,
+  Input,
+  Label,
+  NavLink,
+  Checkbox
+} from "theme-ui";
+import { Router, Link } from "@reach/router";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { IdentityContext } from "../../identity-context";
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    todos: [Todo]!
-  }
-  type Todo {
-    id: ID!
-    text: String!
-    done: Boolean!
-  }
-  type Mutation {
-    addTodo(text: String!): Todo
-    updateTodoDone(id: ID!): Todo
+const ADD_TODO = gql`
+  mutation AddTodo($text: String!) {
+    addTodo(text: $text) {
+      id
+    }
   }
 `;
 
-const todos = {};
-let todoIndex = 0;
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    todos: () => {
-      return Object.values(todos);
+const UPDATE_TODO_DONE = gql`
+  mutation UpdateTodoDone($id: ID!) {
+    updateTodoDone(id: $id) {
+      text
+      done
     }
-  },
-  Mutation: {
-    addTodo: (_, { text }) => {
-      todoIndex++;
-      const id = `key-${todoIndex}`;
-      todos[id] = { id, text, done: false };
-      return todos[id];
-    },
-    updateTodoDone: (_, { id }) => {
-      todos[id].done = true;
-      return todos[id];
+  }
+`;
+
+const GET_TODOS = gql`
+  query GetTodos {
+    todos {
+      id
+      text
+      done
     }
+  }
+`;
+
+const todosReducer = (state, action) => {
+  switch (action.type) {
+    case "addTodo":
+      return [{ done: false, value: action.payload }, ...state];
+    case "toggleTodoDone":
+      const newState = [...state];
+      newState[action.payload] = {
+        done: !state[action.payload].done,
+        value: state[action.payload].value
+      };
+      return newState;
   }
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-
-  // By default, the GraphQL Playground interface and GraphQL introspection
-  // is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
-  //
-  // If you'd like to have GraphQL Playground and introspection enabled in production,
-  // the `playground` and `introspection` options must be set explicitly to `true`.
-  playground: true,
-  introspection: true
-});
-
-exports.handler = server.createHandler();
+export default () => {
+  const { user, identity: netlifyIdentity } = useContext(IdentityContext);
+  const [todos, dispatch] = useReducer(todosReducer, []);
+  const inputRef = useRef();
+  const [addTodo] = useMutation(ADD_TODO);
+  const [updateTodoDone] = useMutation(UPDATE_TODO_DONE);
+  const { loading, error, data, refetch } = useQuery(GET_TODOS);
+  return (
+    <Container>
+      <Flex as="nav">
+        <NavLink as={Link} to="/" p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to={"/app"} p={2}>
+          Dashboard
+        </NavLink>
+        {user && (
+          <NavLink
+            href="#!"
+            p={2}
+            onClick={() => {
+              netlifyIdentity.logout();
+            }}
+          >
+            Log out {user.user_metadata.full_name}
+          </NavLink>
+        )}
+      </Flex>
+      <Flex
+        as="form"
+        onSubmit={async e => {
+          e.preventDefault();
+          await addTodo({ variables: { text: inputRef.current.value } });
+          inputRef.current.value = "";
+          await refetch();
+        }}
+      >
+        <Label sx={{ display: "flex" }}>
+          <span>Add&nbsp;Todo</span>
+          <Input ref={inputRef} sx={{ marginLeft: 1 }} />
+        </Label>
+        <Button sx={{ marginLeft: 1 }}>Submit</Button>
+      </Flex>
+      <Flex sx={{ flexDirection: "column" }}>
+        {loading ? <div>loading...</div> : null}
+        {error ? <div>{error.message}</div> : null}
+        {!loading && !error && (
+          <ul sx={{ listStyleType: "none" }}>
+            {data.todos.map(todo => (
+              <Flex
+                key={todo.id}
+                as="li"
+                onClick={async () => {
+                  console.log("updateTodoDone");
+                  await updateTodoDone({ variables: { id: todo.id } });
+                  console.log("refetching");
+                  await refetch();
+                }}
+              >
+                <Checkbox checked={todo.done} readOnly />
+                <span>{todo.text}</span>
+              </Flex>
+            ))}
+          </ul>
+        )}
+      </Flex>
+    </Container>
+  );
+};
--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,3 +1,24 @@
+const React = require("react");
+const {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache
+} = require("@apollo/client");
 const wrapRootElement = require("./wrap-root-element");
 
-exports.wrapRootElement = wrapRootElement;
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri:
+      "https://dazzling-feynman-41a653.netlify.app/.netlify/functions/graphql"
+  })
+});
+
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <ApolloProvider client={client}>
+      {wrapRootElement({ element })}
+    </ApolloProvider>
+  );
+};
